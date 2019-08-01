a97d255d6634c74c1ce21ab4d41601b3cdaabbb1
meta-vinod-sharma <vinod.sharma@metacube.com>
2019-08-01 20:14:15 +0530

    dsaSSihnment2

---
+++ DSAssignment2/ques2/Queue.java
@@ -0,0 +1,15 @@
+package ques2;
+
+public interface Queue {
+
+	
+	public boolean addElementToQueue(int data);
+	public boolean deQueue();
+	public boolean emptyQueue();
+	public boolean fullQueue();
+	public void show();
+
+	
+	
+	
+}
---
+++ DSAssignment2/ques2/QueueOperations.java
@@ -0,0 +1,88 @@
+package ques2;
+
+public class QueueOperations implements Queue {
+
+	int queue[];
+	int size, front, rear;
+
+	public QueueOperations(int size) {
+		this.front = -1;
+		this.rear = -1;
+		this.size = size;
+		queue = new int[size];
+	}
+
+	@Override
+	public boolean addElementToQueue(int data) {
+		if (fullQueue()) {
+			System.out.println("Queue is full");
+			return false;
+		} else if (front == -1) {
+			front = rear = 0;
+			queue[rear] = data;
+		}
+
+		else if (rear == size - 1 && front != 0) {
+			rear = 0;
+			queue[rear] = data;
+		}
+
+		else {
+			rear++;
+			queue[rear] = data;
+		}
+		return true;
+
+	}
+
+	@Override
+	public boolean deQueue() {
+		if (emptyQueue()) {
+			System.out.println("Queue is Empty");
+			return false;
+		} else {
+			queue[front] = -1;
+			if (front == rear) {
+				front = -1;
+				rear = -1;
+			} else if (front == size - 1)
+				front = 0;
+			else
+				front++;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean emptyQueue() {
+		if (front == -1) {
+			return true;
+		}
+		return false;
+
+	}
+
+	@Override
+	public boolean fullQueue() {
+		if ((front == rear + 1) || (front == 0 && rear == size - 1))
+			return true;
+		return false;
+	}
+
+	public void show() {
+		if (front == -1) {
+			System.out.println("\nQueue is Empty");
+		}
+		if (rear >= front) {
+			for (int i = front; i <= rear; i++)
+				System.out.println("-->" + queue[i]);
+		} else {
+			for (int i = front; i < size; i++)
+				System.out.println("-->" + queue[i]);
+
+			for (int i = 0; i <= rear; i++)
+				System.out.println("-->" + queue[i]);
+		}
+	}
+
+}
