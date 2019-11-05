acfcd6249163b022a7edd6d2a8cece818391e9e2
meta-vinod-sharma <vinod.sharma@metacube.com>
2019-11-05 17:19:54 +0530

    UI SESSION 11

---
+++ UI-Session-11-Assignment/REST-API/src/Controller/RestController.java
@@ -0,0 +1,31 @@
+/*
+ * Controller for RestApi
+ */
+package Controller;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.Item;
+import utility.Helper;
+
+@Path("/items")
+public class RestController {
+
+	/**
+	 * Gets All Items
+	 * @return list ofitems as json
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Item> getBooks(){
+		List<Item> books = Helper.getAllItems(); 
+		return books;
+	}
+
+
+}
